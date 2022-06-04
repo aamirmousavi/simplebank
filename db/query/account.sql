8d4828b650b76f6ceca0f2b90336425c41968cb1
aamirmousavi <amirmousavi0003@gmail.com>
2022-06-04 20:04:02 +0430

    AddAccountBalance

--- db/query/account.sql
+++ db/query/account.sql
@@ -17,6 +17,12 @@ SELECT * FROM accounts
 WHERE id = $1 LIMIT 1
 FOR NO KEY UPDATE;
 
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + $2
+WHERE id = $1
+RETURNING *;
+
 -- name: ListAccounts :many
 SELECT * FROM accounts
 ORDER BY id
